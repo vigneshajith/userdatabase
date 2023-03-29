63b1776e766756255b31ccf32fa2402885b55351
vigneshajith <112766532+vigneshajith@users.noreply.github.com>
2023-03-29 18:01:03 +0530

    Update database.config.ts

--- backend/src/configs/database.config.ts
+++ backend/src/configs/database.config.ts
@@ -2,13 +2,14 @@ import { connect } from 'mongoose'
 
 export const dbConnect = () => {
     // For mongodb Atlas
-    // connect(process.env.MONGO_URL!).then(
-    //     () => console.log("mongodb connected"),
-    //     (err) => console.log(err)
-    // )
-    connect("mongodb://127.0.0.1:27017").then(
+    connect(process.env.MONGO_URL!).then(
         () => console.log("mongodb connected"),
         (err) => console.log(err)
     )
+    For mongodb local
+//     connect("mongodb://127.0.0.1:27017").then(
+//         () => console.log("mongodb connected"),
+//         (err) => console.log(err)
+//     )
     
 }
